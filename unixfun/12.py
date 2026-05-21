0c805a3686b0c819f486873aadf2349cedbd4ac0
Ridhi <rsrikanth@ucsd.edu>
2026-05-21 11:27:04 -0700

    ran docker koala + added new transpiled files

---
+++ unixfun/12.py
@@ -0,0 +1,1 @@
+cat $1 $| tr " " "\\n" $| grep "\\." $| cut -d "." -f 2 $| cut -c "1-1" $| tr "[a-z]" P $| sort -r $| uniq $| head -n 3 $| tail -n 1
---
+++ unixfun/19.py
@@ -0,0 +1,1 @@
+cat $1 $| grep Bell $| awk length <= 45 $| cut -d , -f 2 $| awk "{$1=$1};1"
